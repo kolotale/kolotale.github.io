2868cf27758c99695414013fc4d204d0cecabae2
Alexei Fedotov <alexei.fedotov@gmail.com>
2018-11-20 19:44:26 +0300

    added music

--- js/sound.js
+++ js/sound.js
@@ -4,6 +4,14 @@ Sound.state = 0
 
 Sound.controlSound = function() {
     Sound.state = (Sound.state + 1) % 3
+    var elem = document.getElementById('audio')
+    if (Sound.state == 1) {
+        //elem.src = 'http://66.90.93.122/ost/battlefield-2-sandbox-mod-ost/nekijvoasq/Rollercoaster.mp3'
+        elem.src = 'http://66.90.93.122/ost/need-for-speed-most-wanted/mtmwxozr/01-%3D-nine%20thou%20%28superstars%20remix%29.mp3'
+        elem.play()
+    } else if (Sound.state == 0) {
+        elem.pause()
+    }
     UI.showSoundState()
 }
 
