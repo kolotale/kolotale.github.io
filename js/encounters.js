cf4aa7819c704528bb2ed86520c267329d5caf81
Alexei Fedotov <alexei.fedotov@gmail.com>
2018-11-26 18:40:01 +0300

    implemented EncounterMonster

--- js/encounters.js
+++ js/encounters.js
@@ -1,13 +1,65 @@
-Encounters = {}
-
 EncountersList = []
 
 class Encounter {
-    constructor(name) {
-        this.name = name
+    constructor(generalName, name) {
         EncountersList.push(this)
-    }    
+    }
+    
+    init() {
+        this.stage = 0
+    }
 }
 
-new Encounter('Rabbit')
-new Encounter('Wolf')
+class EncounterMonster extends Encounter {
+    constructor(generalName, name) {
+        super()
+        this.generalName = generalName
+        this.name = name
+    }
+    
+    fire() {
+        var generalName = this.generalName
+        var name = this.name
+
+        switch (this.stage) {
+            case 0:
+                UI.notify(Game.getRndElem([
+                    `On and on’ it rolled, and it met a ${generalName} coming toward it.`,
+                    `By and by it met a ${generalName} coming toward it.`]))
+                this.stage = 1
+                break
+            case 1:
+                UI.notify(Game.getRndElem([
+                    `“I’m going to eat you up, Kolobok!” called the ${generalName}.`,
+                    `“I’m going to eat you up, Little Round Bun!” called the ${generalName}.`]))
+                this.stage = 2
+                break
+            case 2:
+                UI.notify(Game.getRndElem([
+                    `“Don’t do that, ${name}, let me sing you a song instead.”`,
+                    `“Please, don’t do that, ${name}, let me sing you a song instead.”`]))
+                this.stage = 3
+                break
+            case 3:
+                UI.notify(Game.getRndElem([
+                    `“All right, let’s hear it!”`]))
+                this.stage = 4
+                break
+            case 4:
+                UI.notify(Game.getRndElem([`“I was scraped from the flour-box<br/>And swept from the bin<br/>And baked in the oven<br/>And cooled on the sill.<br/>I ran away from Grandpa,<br/>I ran away from Grandma,<br/>And I'll run away from you, this minute I will!”`]))
+                this.stage = 5
+                break
+            default:
+                UI.notify(Game.getRndElem([
+                    `And away it rolled and away!`,
+                    `And off it rolled and away.`]))
+                this.stage = 0
+                return null
+        }
+        return this
+    }
+}
+
+new EncounterMonster('Rabbit', 'Fleet-Feet')
+new EncounterMonster('Wolf', 'Brother Wolf')
+new EncounterMonster('Bear', 'Brother Bear')
--- js/events.js
+++ js/events.js
@@ -3,7 +3,7 @@ Events = {}
 Events.PARENTS = ['an old man and an old woman', 'two transgenders', 'two old school gamers', 'two old men', 'a giraffe and a hippopotamus', 'a physisist and a mathematician', 'two spacefarers', 'a knight and a priest']
 Events.PARENT = ['the old man', 'the stronger transgender', 'the strategy gamer', 'the manly man', 'the hippo', 'the theorist', 'the captain', 'the priest']
 Events.PARENT2 = ['the old woman', 'the prettier transgender', 'the arcade gamer', 'the feminine man', 'the giraffe', 'the applied science guy', 'the knowledge keeper', 'the knight']
-   
+
 class EventStr {
     constructor(name, followup = null) {
         this.name = name
@@ -24,7 +24,7 @@ Events.plagueLice = new EventStr('[-]All the dust throughout the land became lic
 
 Events.plagueSwarm = new EventStr('[-]A swarm came harming crops.')
 
-Events.plagueLivestock = new EventStr('[-]A wind brought a terrible plague on livestock in the field on horses and donkeys and camels and on cattle and sheep and goats.')   
+Events.plagueLivestock = new EventStr('[-]A wind brought a terrible plague on livestock in the field on horses and donkeys and camels and on cattle and sheep and goats.')
 
 class EventGetPoor {
     fire() {
@@ -44,7 +44,7 @@ class EventScrapedParent {
     fire() {
         UI.notify('[+]So ' + Events.PARENT2[Player.props.parents] + ' scraped out the flour-box and swept out the bin, made some dough and shaped a little round bun out of it. They lit the oven, baked the bun and put it on the window sill to cool. But the bun, who proclaimed itself as “Kolobok”, jumped out of the window and onto the bench outside, and from the bench onto the ground, and away it rolled along the road!')
         return [new EventChain([...Events.K_STORY, new EventEnd()])]
-    }    
+    }
 }
 
 class EventChain {
@@ -55,7 +55,7 @@ class EventChain {
     fire() {
         var cur = this.chain[0]
         this.chain.splice(0, 1)
-        
+
         if (typeof cur == 'string') {
             UI.notify(cur)
         } else {
@@ -65,59 +65,29 @@ class EventChain {
                 this.chain = cur
             }
         }
-        
+
         return [(this.chain.length > 1) ? new EventChain(this.chain) : this.chain[0]]
     }
 }
 
 class EventEncounter {
-    constructor() {
-        this.stage = 0
-    }
-    
     fire() {
-        switch (this.stage) {
-            case 0:
-                this.encounter = Game.getAndDelRndElem(Game.encounters)
-                var name = this.encounter.name
-                UI.notify(Game.getRndElem([
-                    `On and on’ it rolled, and it met a ${name} coming toward it.`,
-                    `By and by it met a ${name} coming toward it.`]))
-                this.stage = 1
-                return [this]
-            case 1:
-                UI.notify('stage = 1')
-                return null
-            default:
-                return null
+        if (!this.encounter) {
+            this.encounter = Game.getAndDelRndElem(Game.encounters)
+            this.encounter.init()
         }
-    }    
+        
+        var res = this.encounter.fire()
+        if (res) return [this]
+    }
 }
 
 Events.K_STORY = [new EventEncounter(),
-    '“I’m going to eat you up, Kolobok!” called the Rabbit.',
-    '“Don’t do that, Fleet-Feet, let me sing you a song instead,” said Kolobok.',
-    '“All right, let’s hear it!”',
-    'Here it is! “I was scraped from the flour-box...”',
-    '“... And I’ll run away from you, this minute I will!”',
-    'And off it rolled and away.',
-    'By and by it met a Wolf coming toward it.',
-    '“I’m going to eat you up, Little Round Bun!” called the Wolf.',
-    '“Don’t do that, Brother Wolf, let me sing you a song instead.”',
-    '“All right, let’s hear it!”',
-    '“I was scraped from the flour-box...”',
-    '“... And I’ll run away from you, this minute I will!”',
-    'And away it rolled.',
-    'By and by it met a Bear coming toward it.',
-    '“I’m going to eat you up, Kolobok!” called the Bear.',
-    '“Don’t do that, Brother Bear, I’ll sing you a song instead!”',
-    '“All right, let’s hear it!”',
-    '“I was scraped from the flour-box...”',
-    '“... And I’ll run away from you, this minute I will!”',
-    'And away it rolled and away!',
+    new EventEncounter(),
+    new EventEncounter(),
     'By and by it met a Fox coming toward it.',
     '“I’m going to eat you up, Kolobok!” called the Fox.',
-    '"“Don’t do that, Sister Fox, I’ll sing you a song instead.”',
+    '“Don’t do that, Sister Fox, I’ll sing you a song instead.”',
     'All right, let’s hear it!',
     '“I was scraped from the flour-box...”',
     '“... And I’ll run away from you, this minute I will!”',
@@ -131,14 +101,14 @@ class EventEnd {
         Player.weight = 0
         UI.notify('[-] Ouch.')
         return null
-    }    
+    }
 }
 
 Events.init = function() {
     Player.props.parents = Game.getRnd(Events.PARENTS.length)
     UI.notify('Once upon a time there lived ' + Events.PARENTS[Player.props.parents] + ' who were very poor and had nothing at all to their name. <a>Restart</a>?')
     UI.notify('This was a hard year.')
-    
+
     Game.followups = [new EventGetPoor()]
     Game.ambient = [Events.plagueWater, Events.plagueFrogs, Events.plagueLice, Events.plagueSwarm, Events.plagueLivestock]
     Game.encounters = EncountersList.slice()
